23fe4cb1804757a11e3789b129b8b2a907f95b3a
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-10-23 09:11:05 +0200

    init

---
+++ precache-manifest.484d08077f344f1c8a884586aea67d1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8be95886b5015676c9",
+    "url": "/css/app.ba629074.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8cd7e30625f69b65d02e8c07c0d26b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a5831f4e54ff48bc0d",
+    "url": "/js/about.e97b8d0b.js"
+  },
+  {
+    "revision": "dd8be95886b5015676c9",
+    "url": "/js/app.24137c91.js"
+  },
+  {
+    "revision": "18a8dff35c2ee226a844",
+    "url": "/js/chunk-vendors.d47cf30f.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
